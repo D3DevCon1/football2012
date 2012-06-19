d737875989f764d2fb6654406273f1bf593a24fc
John Paul Mc Feely <jaypee007@hotmail.com>
2012-06-19 21:54:22 +0100

    Added predction and scorecast models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Football::Application.routes.draw do
+  resources :scorecasts
+
+  resources :predictions
+
   root :to => 'pages#home'
   
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
