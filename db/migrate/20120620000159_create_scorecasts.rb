393a4be61ca2c7492dd6d4fcdcf9d661d19f8d00
John Paul <john.mcfeely@invisionwfm.com>
2012-06-20 01:21:37 +0100

    Changed structure of scorecast model by including the fixture_id which had been ommitted and repopulated fixtures for GW1

---
+++ db/migrate/20120620000159_create_scorecasts.rb
@@ -0,0 +1,14 @@
+class CreateScorecasts < ActiveRecord::Migration
+  def change
+    create_table :scorecasts do |t|
+      t.integer :user_id
+      t.integer :gameweek_no
+      t.integer :fixture_no
+      t.integer :home_score
+      t.integer :away_score
+      t.integer :points
+
+      t.timestamps
+    end
+  end
+end
